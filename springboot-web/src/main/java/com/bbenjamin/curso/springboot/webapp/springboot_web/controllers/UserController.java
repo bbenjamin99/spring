c1e32e5d8ab2e498f2ea8585645e8aa488e33f90
bbenjamin99 <bbenjamin4253@gmail.com>
2024-11-22 15:23:44 -0300

    Listas en Thymeleaf

--- springboot-web/src/main/java/com/bbenjamin/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ springboot-web/src/main/java/com/bbenjamin/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -4,16 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.bbenjamin.curso.springboot.webapp.springboot_web.models.User;
+
 @Controller
 public class UserController {
 
     @GetMapping("/details")
     public String details(Model model){
 
+        User user1 = new User("Benjamin", "Benitez");
+        user1.setEmail("bbenjamin@correo.com");
         model.addAttribute("title", "Hello World Springboot ");
-        model.addAttribute("name", "Benjamin");
-        model.addAttribute("lastname", "Benitez");
-        model.addAttribute("email", "bbenjamin@correo.com");
+        model.addAttribute("user", user1);
 
 
         return "details";
--- springboot-web/src/main/java/com/bbenjamin/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ springboot-web/src/main/java/com/bbenjamin/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -1,28 +1,51 @@
 package com.bbenjamin.curso.springboot.webapp.springboot_web.controllers;
 
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bbenjamin.curso.springboot.webapp.springboot_web.models.User;
+import com.bbenjamin.curso.springboot.webapp.springboot_web.models.dto.UserDto;
+
+
 
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
 
-    @GetMapping("/details2")
-    public Map<String,Object> details(){
-        Map<String, Object> body = new HashMap<>();
+    @GetMapping("/details")
+    public UserDto details(){
+        UserDto userDto = new UserDto();
+        User user1 = new User("Benjamin", "Benitez");
+        userDto.setUser(user1);
+        userDto.setTitle("Hello World Springboot");
+        return userDto;
+    }
 
+    @GetMapping("/list")    
+    public List<User> list(){
+        User benja = new User("Benjamin", "Benitez");
+        benja.setEmail("bbenjamin@correo.com");
+        User mica = new User("Micaela", "De Luca");
+        User rocco = new User("Rocco", "De Luca");
+
+        List<User> users = Arrays.asList(benja,mica,rocco);
+        return users;
+    }
+
+    @GetMapping("/details-map")
+    public Map<String,Object> details2(){
+        Map<String, Object> body = new HashMap<>();
+        User user1 = new User("Benjamin", "Benitez");
         body.put("title", "Hello World Springboot ");
-        body.put("name", "Benjamin");
-        body.put("lastname", "Benitez");
-        body.put("email", "bbenjamin@correo.com");
+        body.put("user", user1);
 
         return body;
-        
     }
 }
