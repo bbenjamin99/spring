d9aa63da5db73feb53715ebe6f8127071f0a647f
bbenjamin99 <bbenjamin4253@gmail.com>
2024-11-24 13:01:23 -0300

    Anotacion RequestParam

--- springboot-web/src/main/java/com/bbenjamin/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ springboot-web/src/main/java/com/bbenjamin/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,8 +1,14 @@
 package com.bbenjamin.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.bbenjamin.curso.springboot.webapp.springboot_web.models.User;
 
@@ -16,8 +22,23 @@ public class UserController {
         user1.setEmail("bbenjamin@correo.com");
         model.addAttribute("title", "Hello World Springboot ");
         model.addAttribute("user", user1);
-
-
         return "details";
+    }
+
+    @GetMapping("/list")
+    public String list(ModelMap model){
+
+        model.addAttribute("title", "Users lists");
+        return "list";
+    }
+
+    @ModelAttribute("users")
+    public List<User> usersModel() {
+
+        List<User> users = Arrays.asList(new User("Benjamin", "Benitez", "bbenjamin@correo.com")
+        , new User("Micaela", "De Luca", "dlmicaela@correo.com")
+        , new User("Ronalda", "De Luca", "dlronalda@correo.com"));
+
+        return users;
     }
 }
