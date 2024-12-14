5544104398f8b861df0283fdcd72ce1e906ae211
bbenjamin99 <bbenjamin4253@gmail.com>
2024-12-14 11:23:21 -0300

    Anotacion PathVariable

--- springboot-web/src/main/java/com/bbenjamin/curso/springboot/webapp/springboot_web/controllers/RequestParamsController.java
+++ springboot-web/src/main/java/com/bbenjamin/curso/springboot/webapp/springboot_web/controllers/RequestParamsController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bbenjamin.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
 import com.bbenjamin.curso.springboot.webapp.springboot_web.models.dto.ParamMixDto;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/api/params")
 public class RequestParamsController {
@@ -32,4 +34,23 @@ public class RequestParamsController {
         return params;
     }
 
-}
+     @GetMapping("/request")
+     public ParamMixDto request(HttpServletRequest request) {
+ 
+        Integer code =0;
+        try{
+           code = Integer.parseInt(request.getParameter("code"));
+
+        }catch(NumberFormatException e ){
+            throw e;
+        }
+        ParamMixDto params = new ParamMixDto();
+        params.setCode(code);
+        params.setMessage(request.getParameter("message"));
+        return params;
+        
+        }
+     }
+     
+
+
